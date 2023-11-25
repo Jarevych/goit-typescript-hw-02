8cd5d424c942f27df6032adcc1f73bcf8044c3d6
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-11-25 23:49:51 +0200

    half generics

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,16 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
+interface CompProps {
+  title: string
+}
+class Component<T extends CompProps> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <{ title: string}> {
   pageInfo () {
     console.log(this.props.title);
   }
